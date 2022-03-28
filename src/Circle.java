91eba8d914d710d70acdbd7c72a1939bd459ab58
Loc <daoloc93@gmail.com>
2022-03-28 16:36:15 +0700

    first commit

---
+++ src/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+    private double radius = 1;
+    private String color = "red";
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return Math.PI * this.radius;
+    }
+}
